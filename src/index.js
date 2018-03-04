4c8869d2ebd8f52f83a04cb60925259ae7fa5400
alex.gvozden <alex.gvozden@gmail.com>
2018-03-04 13:15:52 +0100

    form validation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import csrf from 'csurf';
 
+import { isURL } from './utils/URL';
+
 const PORT = 3000;
 
 import DB from './db';
@@ -18,6 +20,15 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// error handling for token
+app.use(function(err, req, res, next) {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  // handle CSRF token errors here
+  res.status(403);
+  res.send('form tampered with');
+});
+
 const db = new DB();
 
 async function start() {
@@ -27,15 +38,50 @@ async function start() {
 start();
 
 // create new url
-app.post('/url', csrfProtection, (req, res) => {
-  console.log('submitted url ', req.body.url);
+app.post('/', csrfProtection, (req, res) => {
+  let vars = {
+    csrfToken: req.csrfToken(),
+    errorUrl: false,
+    errorAlias: false,
+    url: '',
+    alias: ''
+  };
 
-  res.send(JSON.stringify({ success: true }));
+  // check for proper submitted url
+  if (!req.body.url || !isURL(req.body.url)) {
+    vars.errorUrl = true;
+  }
+
+  // if alias is sent, check if it contains only letters number and hyphen
+  if (req.body.alias && !/^[aA-zZ0-9-]+$/g.test(req.body.alias)) {
+    vars.errorAlias = true;
+  }
+
+  // return results if any error is detected
+  if (vars.errorUrl || vars.errorAlias) {
+    // assign previous values so user can correct errors
+    vars.url = req.body.url;
+    vars.alias = req.body.alias;
+    res.render('home', vars);
+  }
+
+  // process url and create new short url
+
+  // render the page
+  res.render('home', vars);
 });
 
 // serve index page
 app.get('/', csrfProtection, function(req, res) {
-  res.render('home', { csrfToken: req.csrfToken() });
+  let vars = {
+    csrfToken: req.csrfToken(),
+    errorUrl: false,
+    errorAlias: false,
+    url: '',
+    alias: ''
+  };
+
+  res.render('home', vars);
   // res.sendFile(path.join(__dirname + '/index.html'));
 });
 
