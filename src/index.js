b65c30477d5cd9e61405083b66c7d60b7dc1db3c
alex.gvozden <alex.gvozden@gmail.com>
2018-03-04 12:01:42 +0100

    added ejs templates and csfr protection

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ require('babel-polyfill');
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import csrf from 'csurf';
 
 const PORT = 3000;
 
 import DB from './db';
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+const csrfProtection = csrf({ cookie: true });
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -21,15 +27,16 @@ async function start() {
 start();
 
 // create new url
-app.post('/url', (req, res) => {
+app.post('/url', csrfProtection, (req, res) => {
   console.log('submitted url ', req.body.url);
 
   res.send(JSON.stringify({ success: true }));
 });
 
 // serve index page
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', csrfProtection, function(req, res) {
+  res.render('home', { csrfToken: req.csrfToken() });
+  // res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT);
