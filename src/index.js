c7232c7d1fc0b2ca21d911d605db33895242a8de
alex.gvozden <alex.gvozden@gmail.com>
2018-03-04 11:02:41 +0100

    async await support added plus PORT as constant

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import path from "path";
-import express from "express";
-import bodyParser from "body-parser";
+require('babel-polyfill');
 
-import DB from "./db";
+import path from 'path';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const PORT = 3000;
+
+import DB from './db';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,19 +14,23 @@ app.use(bodyParser.json());
 
 const db = new DB();
 
-db.init();
+async function start() {
+  const dbInit = await db.init();
+}
+
+start();
 
 // create new url
-app.post("/url", (req, res) => {
-  console.log("submitted url ", req.body.url);
+app.post('/url', (req, res) => {
+  console.log('submitted url ', req.body.url);
 
   res.send(JSON.stringify({ success: true }));
 });
 
 // serve index page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
-console.log("Listening on 8080");
+app.listen(PORT);
+console.log(`Listening on ${PORT}`);
